793ec47e49aca6ed6cdbf80d43f66140cd29f252
daggzu <diegog8603@gmail.com>
2023-11-30 15:02:18 -0400

    changing innit

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-import os
-
-type_storage = os.getenv('HBNB_TYPE_STORAGE')
+"""Instantiates a storage object."""
+from os import getenv
 
 
-if type_storage == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
