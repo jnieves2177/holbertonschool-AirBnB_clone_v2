d8cda8e95b446fbb8847baee7a82b84f394ed054
daggzu <diegog8603@gmail.com>
2023-11-29 21:17:38 -0400

    testing somethi g

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,4 +55,4 @@ class FileStorage:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
-                print("OK")
+                print("OK", end="")
