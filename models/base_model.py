794ce666a42a5021e480d3554ae025b7e9b7db16
daggzu <diegog8603@gmail.com>
2023-12-01 17:16:47 -0400

    fixing base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,19 +4,23 @@ import models
 from uuid import uuid4
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, DateTime, String
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
 
 Base = declarative_base()
 
-class BaseModel(Base):
+
+class BaseModel:
     """Defines the BaseModel class."""
 
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel."""
+
         self.id = str(uuid4())
         self.created_at = self.updated_at = datetime.utcnow()
         if kwargs:
@@ -28,12 +32,14 @@ class BaseModel(Base):
 
     def save(self):
         """Update updated_at with the current datetime."""
+
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance."""
+
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
@@ -47,6 +53,7 @@ class BaseModel(Base):
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
+
         d = self.__dict__.copy()
         d.pop("_sa_instance_state", None)
         return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
