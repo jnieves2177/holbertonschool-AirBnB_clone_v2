74c244252306c38c424c8efda56c6e756bdca76d
daggzu <diegog8603@gmail.com>
2023-12-14 19:19:05 -0400

    ray rizzo

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)  # Holds the name of the module.
+
+
+@app.route('/', strict_slashes=False)  # Maps the root route.
+def hello_hbnb():
+    """Function returns a string when routed to"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)  # Maps the /hbnb route.
+def hbnb():
+    """Function returns a string when routed to"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)  # Maps the /c/<text> route.
+def c(text):
+    """Function returns a string when routed to"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Runs the application.
